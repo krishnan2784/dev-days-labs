2ab4db6dba0660e7ff336b794e28535ac49e7e66
Krishnan Nath <krishnan2784@gmail.com>
2016-12-09 16:58:39 +0000

    Add the functionality for seeing speaker details

---
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/Services/EmotionService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+namespace DevDaysSpeakers
+{
+	public class EmotionService
+	{
+		private static async Task<Emotion[]> GetHappinessAsync(string url)
+		{
+			var client = new HttpClient();
+			var stream = await client.GetStreamAsync(url);
+			var emotionClient = new EmotionServiceClient("25b11727cd2f43dfbf3c99579bbb776d");
+
+			var emotionResults = await emotionClient.RecognizeAsync(stream);
+
+			if (emotionResults == null || emotionResults.Count() == 0)
+			{
+				throw new Exception("Can't detect face");
+			}
+
+			return emotionResults;
+		}
+
+		//Average happiness calculation in case of multiple people
+		public static async Task<float> GetAverageHappinessScoreAsync(string url)
+		{
+			Emotion[] emotionResults = await GetHappinessAsync(url);
+
+			float score = 0;
+			foreach (var emotionResult in emotionResults)
+			{
+				score = score + emotionResult.Scores.Happiness;
+			}
+
+			return score / emotionResults.Count();
+		}
+
+		public static string GetHappinessMessage(float score)
+		{
+			score = score * 100;
+			double result = Math.Round(score, 2);
+
+			if (score >= 50)
+				return result + " % :-)";
+			else
+				return result + "% :-(";
+		}
+	}
+}
--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/View/SpeakersPage.xaml.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/View/SpeakersPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Xamarin.Forms;
 using DevDaysSpeakers.ViewModel;
 using System;
+using DevDaysSpeakers.Model;
 
 namespace DevDaysSpeakers.View
 {
@@ -17,9 +18,15 @@ namespace DevDaysSpeakers.View
 			ListViewSpeakers.ItemSelected += ListViewSpeakers_ItemSelected;
 		}
 
-		void ListViewSpeakers_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		private async void ListViewSpeakers_ItemSelected(object sender, SelectedItemChangedEventArgs e)
 		{
-			throw new NotImplementedException();
+			var speaker = e.SelectedItem as Speaker;
+			if (speaker == null)
+				return;
+
+			await Navigation.PushAsync(new DetailsPage(speaker));
+
+			ListViewSpeakers.SelectedItem = null;
 		}
 }
 }
